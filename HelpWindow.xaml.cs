95dc25dc69f6719f384be60767ae341995333cc1
Kenneth Chen <74205780+Chenneth@users.noreply.github.com>
2022-10-21 19:51:09 -0500

    Latest version upload

---
+++ HelpWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace AcctISGenerator;
+
+public partial class HelpWindow
+{
+    public HelpWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri){UseShellExecute = true});
+        e.Handled = true;
+    }
+}
